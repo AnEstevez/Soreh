56577a978ac4fc7009d9e526c4053c8678513204
AnEstevez <andres.estevez.pro@gmail.com>
2023-05-11 17:57:05 +0200

    timber

---
+++ app/src/main/java/com/andresestevez/soreh/App.kt
@@ -0,0 +1,36 @@
+package com.andresestevez.soreh
+
+import android.app.Application
+import timber.log.Timber
+
+class App: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        Timber.plant(object : Timber.DebugTree() {
+            /**
+             * Override [log] to modify the tag and add a "global tag" prefix to it. You can rename the String "global_tag_" as you see fit.
+             */
+            override fun log(
+                priority: Int, tag: String?, message: String, t: Throwable?
+            ) {
+                super.log(priority, "timber_$tag", message, t)
+            }
+
+            /**
+             * Override [createStackElementTag] to include a append a "method name" to the tag.
+             */
+            override fun createStackElementTag(element: StackTraceElement): String {
+                return String.format(
+                    "%s:%s:%s",
+                    element.fileName,
+                    element.methodName,
+                    element.lineNumber,
+                    super.createStackElementTag(element)
+                )
+            }
+        })
+    }
+
+}
